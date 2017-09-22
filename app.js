1b9f546cfd0b74e2cad51825c4cbf3c6e1bd7fce
Justin Brown <brown.jt@gmail.com>
2017-09-22 09:39:34 -0600

    Copied repo from [spike-test](https://bitbucket.org/triggerinteractive/spike-test).

---
+++ app.js
@@ -0,0 +1,34 @@
+const Records = require('spike-records')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const locals = {}
+
+module.exports = {
+  devtool: 'source-map',
+  ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    locals: () => locals
+  }),
+  postcss: cssStandards(),
+  babel: jsStandards(),
+  plugins: [new Records({
+    addDataTo: locals,
+    artists: { 
+      url: 'http://localhost:8080/api/1.1/tables/artist/rows?access_token=92g3J2AynDXvoMkkUzVkCs9CeCXLPhrt',
+      transform: (response) => { return response.data },
+      template: {
+        path: 'views/artist.html',
+        output: (artist) => { return `artists/${artist.name}.html` }
+      }
+    },
+    labels: { 
+      url: 'http://localhost:8080/api/1.1/tables/label/rows?access_token=92g3J2AynDXvoMkkUzVkCs9CeCXLPhrt',
+      transform: (response) => { return response.data },
+      template: {
+        path: 'views/label.html',
+        output: (label) => { return `labels/${label.slug}.html` }
+      }
+    }
+  })]
+}
