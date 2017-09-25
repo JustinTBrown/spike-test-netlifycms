95693f67142b75567c4a01486d23617f9425ebaa
Justin Brown <brown.jt@gmail.com>
2017-09-25 15:07:22 -0600

    Rework project structure.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const Records = require('spike-records')
-const Collections = require('spike-collections')
+// const Collections = require('spike-collections')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const locals = {}
-const collections = new Collections({ 
-  addDataTo: locals, 
-  collections: {
-    posts: { files: 'pages/**' }
-  }
-})
+// const collections = new Collections({ 
+//   addDataTo: locals, 
+//   collections: {
+//     posts: { files: 'pages/**' }
+//   }
+// })
 
 module.exports = {
   devtool: 'source-map',
@@ -25,15 +25,15 @@ module.exports = {
       url: 'http://165.227.42.179:8080/api/1.1/tables/artist/rows?access_token=92g3J2AynDXvoMkkUzVkCs9CeCXLPhrt',
       transform: (response) => { return response.data },
       template: {
-        path: 'views/artist.html',
-        output: (artist) => { return `artists/${artist.name}.html` }
+        path: 'templates/artist.html',
+        output: (artist) => { return `artists/${artist.slug}.html` }
       }
     },
     labels: { 
       url: 'http://165.227.42.179:8080/api/1.1/tables/label/rows?access_token=92g3J2AynDXvoMkkUzVkCs9CeCXLPhrt',
       transform: (response) => { return response.data },
       template: {
-        path: 'views/label.html',
+        path: 'templates/label.html',
         output: (label) => { return `labels/${label.slug}.html` }
       }
     }
